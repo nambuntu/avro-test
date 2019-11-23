3ac98ee45362b2376ece95f9e9157e58dfe8e2e8
Nam Nguyen <namnvhue@gmail.com>
2019-11-23 09:13:14 +0100

    share project

---
+++ src/main/java/work/inlumina/avrotest/AvroTestApplication.java
@@ -0,0 +1,12 @@
+package work.inlumina.avrotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroTestApplication.class, args);
+	}
+}
---
+++ src/test/java/work/inlumina/avrotest/AvroTestApplicationTests.java
@@ -0,0 +1,16 @@
+package work.inlumina.avrotest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvroTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
